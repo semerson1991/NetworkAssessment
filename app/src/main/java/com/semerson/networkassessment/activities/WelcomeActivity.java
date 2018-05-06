c7727ba54a68467ff4fdc1f19e3ab8bc8b8de69b
semerson <stevenemerson91@hotmail.co.uk>
2018-05-06 19:17:06 +0100

    Navigation Menu

--- app/src/main/java/com/semerson/networkassessment/activities/WelcomeActivity.java
+++ app/src/main/java/com/semerson/networkassessment/activities/WelcomeActivity.java
@@ -1,13 +1,25 @@
 package com.semerson.networkassessment.activities;
 
+import android.accounts.Account;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.semerson.networkassessment.R;
+import com.semerson.networkassessment.activities.user.awareness.UserAwareness;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +28,74 @@ public class WelcomeActivity extends AppCompatActivity {
 
         final EditText networkName = (EditText) findViewById(R.id.txtUsername);
 
-        final TextView networkNameSelected = (TextView) findViewById(R.id.tvNetworkNameDisplay);
-        networkNameSelected.setText(networkName.getText());
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        switch (menuItem.getItemId()){
+                            case R.id.nav_home:
+                                break;
+                            case R.id.nav_vulnerability_assessment:
+                                Intent activity_vulnScanner = new Intent(WelcomeActivity.this, NetworkScanner.class);
+                                WelcomeActivity.this.startActivity(activity_vulnScanner);
+                                break;
+                            case R.id.nav_sec_awareness:
+                                Intent activity_secAwareness = new Intent(WelcomeActivity.this, UserAwareness.class);
+                                WelcomeActivity.this.startActivity(activity_secAwareness);
+                                break;
+                            case R.id.nav_settings:
+                                Intent activity_settings = new Intent(WelcomeActivity.this, SettingsActivity.class);
+                                WelcomeActivity.this.startActivity(activity_settings);
+                                break;
+                            case R.id.nav_account:
+                                Intent activity_account = new Intent(WelcomeActivity.this, AccountActivity.class);
+                                WelcomeActivity.this.startActivity(activity_account);
+                                break;
+                                default:
+                        }
+                        return true;
+                    }
+                });
+
+
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       // final TextView networkNameSelected = (TextView) findViewById(R.id.tvNetworkNameDisplay);
+        //networkNameSelected.setText(networkName.getText());
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/semerson/networkassessment/service/ServerCommunicationService.java
+++ app/src/main/java/com/semerson/networkassessment/service/ServerCommunicationService.java
@@ -1,14 +1,12 @@
 package com.semerson.networkassessment.service;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.app.ProgressDialog;
 import android.util.Log;
 
 import com.semerson.networkassessment.Utils.ProcessHttpResponse;
 import com.semerson.networkassessment.Utils.RequestBuilder;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -21,32 +19,24 @@ public class ServerCommunicationService extends AsyncTask<String, Integer, Strin
     public static final String BASE_URL = "http://192.168.0.11:8000";
     public static final String URL_RUN_SCAN = BASE_URL+"/perform-scan";
     public static final String URL_REGISTER = BASE_URL+"/register-user/";
-    public static final String URL_REGISTER_NETWORK = BASE_URL+"/register-network-config/";
+    //public static final String URL_REGISTER_NETWORK = BASE_URL+"/register-network-config/";
     public static final String URL_LOGIN = BASE_URL+"/login-user/";
 
-    private Context context;
     private RequestBody requestBody;
-    private ProgressDialog progressDialog;
     private ProcessHttpResponse processHttpResponse;
 
     public ServerCommunicationService(Context theContext) {
         if (theContext instanceof ProcessHttpResponse){
             processHttpResponse = (ProcessHttpResponse) theContext;
         }
-        context = theContext;
         if (theContext instanceof RequestBuilder){
-            requestBody = (RequestBody) ((RequestBuilder) theContext).buildRequestBody();
+            requestBody = ((RequestBuilder) theContext).buildRequestBody();
         }
     }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-
-        progressDialog = new ProgressDialog(context);
-        progressDialog.setTitle("Testing Dialog");
-        progressDialog.setMessage("Testing Dialog");
-        progressDialog.show();
     }
 
     @Override
@@ -79,34 +69,9 @@ public class ServerCommunicationService extends AsyncTask<String, Integer, Strin
     @Override
     protected void onPostExecute(String result) { //called when 'doinbackground' is over .. this will get the String (Downloaded Data) from doInBackground
         super.onPostExecute(result);
-        progressDialog.dismiss();
         if (processHttpResponse != null){
             processHttpResponse.processResponse(result);
         }
-
     }
 
-    public void performRequest(final String url) {
-
-        if (!url.isEmpty()) {
-            switch (url){
-                case URL_RUN_SCAN:
-                    sendRunScanRequest();
-                    break;
-                case URL_REGISTER:
-
-                    break;
-                case URL_LOGIN:
-
-                    break;
-                    default:
-                        break;
-
-            }
-        }
-    }
-
-    private void sendRunScanRequest() {
-
-    }
 }
